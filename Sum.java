efddae0487f392595b3626afcb140c50c10c7a7e
mohammadhemaz <mohammadhemaz46@gmail.com>
2026-02-16 09:28:55 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum
+  {
+    public static void main(String args[])
+    {
+      Scanner s = new Scanner(System.in);
+      int a=s.nextInt();
+      int b=s.nextInt();
+      int sum= a+b;
+      system.out.println("the sum of 2 no.="+sum)
+    }
+  }
